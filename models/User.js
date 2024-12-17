9153cff5d587dfe8bbbff99a77afa8971b205279
alan <alan.266@hotmail.com>
2024-12-17 17:57:09 -0400

    updates

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true },
+  uid: { type: String, required: true, unique: true }, // UID generado por Firebase
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
