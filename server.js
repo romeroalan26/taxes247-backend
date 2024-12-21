cddca036445c72ddcabad84b1cd76065e19c8261
alan <alan.266@hotmail.com>
2024-12-21 16:31:09 -0400

    updates

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Puerto dinámico para Render
 
 // Middlewares
-app.use(cors());
+app.use(cors()); // Configurar CORS para permitir solicitudes desde el frontend
 app.use(express.json());
 app.use(morgan("combined"));
 
@@ -40,6 +40,11 @@ mongoose
     process.exit(1);
   });
 
+// Escuchar en el puerto dinámico
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  if (process.env.NODE_ENV === "production") {
+    console.log(`Servidor corriendo en producción en el puerto ${PORT}`);
+  } else {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  }
 });
