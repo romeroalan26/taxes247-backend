364ce8d407dbbb8532147b0a83fb6a12b845ddcf
Alan <alan.266@hotmail.com>
2025-01-15 21:46:29 -0400

    feat: se divided las DB entre dev y prod

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const adminRoutes = require('./routes/admin.routes');
+const adminRoutes = require("./routes/admin.routes");
 const userRoutes = require("./routes/user.routes");
 const requestRoutes = require("./routes/request.routes");
 
@@ -14,7 +14,13 @@ const PORT = process.env.PORT || 4000; // Puerto dinámico para Render
 // Middlewares
 app.use(cors()); // Configurar CORS para permitir solicitudes desde el frontend
 app.use(express.json());
-app.use(morgan("combined"));
+
+// Configurar logs dependiendo del entorno
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined")); // Logs detallados en producción
+} else {
+  app.use(morgan("dev")); // Logs simplificados en desarrollo
+}
 
 // Rutas
 app.use("/api/admin", adminRoutes);
@@ -27,16 +33,26 @@ app.get("/", (req, res) => {
 });
 
 // Conexión a MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB conectado..."))
-  .catch((error) => {
+const connectDB = async () => {
+  const dbURI =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URI_PROD // URI de la base de datos de producción
+      : process.env.MONGO_URI_DEV; // URI de la base de datos de desarrollo
+
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB conectado en modo ${process.env.NODE_ENV}...`);
+  } catch (error) {
     console.error("Error conectando a MongoDB:", error.message);
-    process.exit(1);
-  });
+    process.exit(1); // Finaliza la aplicación si no se puede conectar
+  }
+};
+
+// Iniciar la conexión
+connectDB();
 
 // Escuchar en el puerto dinámico
 app.listen(PORT, () => {
